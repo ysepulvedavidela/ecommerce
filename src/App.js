be54541fd730877c0b07abc90c42881607888803
ysepulvedavidela <yeremy.sepulveda@gmail.com>
2023-07-18 15:59:22 -0400

    add count button in console

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { NavBar } from './components/NavBar/NavBar';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
+import { ItemCount } from './components/ItemCount/ItemCount';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <ItemListContainer greeting={'Bienvenido a mi nueva tienda'} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <ItemListContainer greeting={'Bienvenidos'}/>
+            <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada', quantity)}/>
+        </div>
+    );
 }
 
 export default App;
